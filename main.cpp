2f1cd4f32d007c2810efc55e8d8a66a6aff2940a
Snej69420 <jens.vissenberg@student.uantwerpen.be>
2022-03-03 15:01:18 +0100

    first

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by jens on 03.03.22.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    cout << "Suffering is the way through life." << endl;
+    return 0;
+}
+
